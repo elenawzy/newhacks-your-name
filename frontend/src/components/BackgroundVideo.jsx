3a289ae328103c1728148b4c938f7b278c8cc517
Chloe Zhao <yichenesia@gmail.com>
2020-11-07 18:11:50 -0500

    Added title / input form for user name

--- frontend/src/components/BackgroundVideo.jsx
+++ frontend/src/components/BackgroundVideo.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 import classes from './BackgroundVideo.module.css';
 import source from "../assets/comet3.mp4";
+import { withStyles } from '@material-ui/core/styles';
 
+
+const WhiteTextField = withStyles({
+    root: {
+      '& .MuiInputBase-input': {
+        color: '#fff', // Text color
+      },
+      '& .MuiInput-underline:before': {
+        borderBottomColor: '#fff8', // Semi-transparent underline
+      },
+      '& .MuiInput-underline:hover:before': {
+        borderBottomColor: '#fff', // Solid underline on hover
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: '#fff', // Solid underline on focus
+      },
+      "& label.Mui-focused": {
+        color: "white"
+      },
+      "& .MuiInputLabel-root": {
+        color: "white",
+        fontSize: 20,
+      },
+    },
+  })(TextField);
 
 const BackgroundVideo = () => {
     return(
@@ -12,10 +38,15 @@ const BackgroundVideo = () => {
             </video>
             
             <div className={classes.Content}>
-                <div className={classes.SubContent} >
-                    <h1>your name.</h1>
-                </div>
+
+            <WhiteTextField autoWidth={true} label="your name."
+
+                // onChange={this.handleNameChange}
+                // value={this.props.name}
+            />
+
             </div>
+
         </div>
     )
 }
