5b96f9129364d6065d7b26e4438b5040c4859d5a
Chloe Zhao <yichenesia@gmail.com>
2020-11-08 01:01:28 -0500

    Added recipient functionality and sending functionality

--- frontend/src/components/SendMail.js
+++ frontend/src/components/SendMail.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import Navigation from "./Navigation";
 import "./Dashboard.css";
 import Editor from './Editor';
-
+import SimpleMenu from "./Menu";
+import Button from '@material-ui/core/Button';
+import {Link, Redirect, withRouter} from 'react-router-dom';
 
 function SendMail() {
   return (
@@ -12,9 +14,13 @@ function SendMail() {
       <Row>
         <Col className="column" xs={12} md={1}>
           <h5>send mail</h5>
+          <SimpleMenu></SimpleMenu>
         </Col>
         <Col className="column" xs={12} md={11}>
+          
           <Editor></Editor>
+          <Link to="/dashboard"><Button>Send</Button></Link>
+          
         </Col>
       </Row>
     </div>
